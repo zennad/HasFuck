6f086e7beacc4b602ba353bd4f5b9088f69853ec
zennad <zennad101@gmail.com>
2020-09-01 14:45:31 +0100

    Create main.hs

---
+++ main.hs
@@ -0,0 +1,12 @@
+
+data BrainFuck = MoveLeft
+               | MoveRight
+               | IncBrain
+               | DecBrain
+               | ShowBrain
+               | ReadBrain
+               | LeftLoop
+               | RightLoop
+               deriving (Eq, Show, Read)
+
+main = undefined
